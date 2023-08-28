918436f38fb36d21daa17f643dcf5c1f25df299d
SimonDesc <pro.descombes@gmail.com>
2023-08-28 10:04:11 +0200

    fix: pycodestyle

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Write a type-annotated function concat that takes a string"""
 
+
 def concat(str1: str, str2: str) -> str:
     """concat 2 str"""
     strcpy = str1 + str2
