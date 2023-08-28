8b4508b134fdb4dd465d2ecf429b936e42c40491
SimonDesc <pro.descombes@gmail.com>
2023-08-28 16:16:50 +0200

    feat: Write a function (do not create an async function, use the regular function syntax to do this) task_wait_random that takes an integer max_delay and returns a asyncio.Task.

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Write an asynchronous coroutine that takes in an integer argument"""
+import random
+import asyncio
+from typing import List
+import time
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int) -> object:
+    """takes an integer max_delay and returns a asyncio.Task"""
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
