e17391fbec9f609df168b8a691166e6e8a5c0cdb
SimonDesc <pro.descombes@gmail.com>
2023-08-28 10:44:07 +0200

    feat: Complex types - string and int/float to tuple

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function sum_list which takes a list"""
+from typing import List, Set, Dict, Tuple, Union
+
+
+def to_kv(k : str, v: Union[int, float]) -> Tuple[str, float]:
+    """return the sum of a list"""
+    return (k, v*v)
