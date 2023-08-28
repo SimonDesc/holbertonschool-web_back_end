87748737947d155e23e07ebabe01b901fa4e7a22
SimonDesc <pro.descombes@gmail.com>
2023-08-28 10:22:24 +0200

    feat: Write a type-annotated function sum_list which takes a list input_list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function sum_list which takes a list"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """return the sum of a list"""
+    return sum(input_list)
