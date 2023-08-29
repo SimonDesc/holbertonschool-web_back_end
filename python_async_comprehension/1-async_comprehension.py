7c60a7c3c035fddb34f059185f30fc0047dc2989
SimonDesc <pro.descombes@gmail.com>
2023-08-29 10:25:23 +0200

    fix: import List from typing

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 """Write a coroutine called async_generator that takes no arguments"""
 
-import asyncio
-import random
-from typing import Generator
+from typing import List
 
 
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """yield a random number between 0 and 10"""
 
     return [i async for i in async_generator()]
