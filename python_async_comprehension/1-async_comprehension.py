2ce93aea44c87197e5a68e9bbfaeaaea52bbeb68
SimonDesc <pro.descombes@gmail.com>
2023-08-29 10:22:31 +0200

    feat: The coroutine will collect 10 random numbers using an async comprehensing over async_generator, then return the 10 random numbers.

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Write a coroutine called async_generator that takes no arguments"""
+
+import asyncio
+import random
+from typing import Generator
+
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> list[float]:
+    """yield a random number between 0 and 10"""
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
