12443e5a1666a7994999b0a7fdd19b9186c6d746
SimonDesc <pro.descombes@gmail.com>
2023-09-12 10:59:52 +0200

    fix: disable eslint

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,8 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
+    const taskInsideBlock = true;
+    // eslint-disable-next-line no-unused-vars
+    const task2InsideBlock = false;
   }
 
   return [task, task2];
