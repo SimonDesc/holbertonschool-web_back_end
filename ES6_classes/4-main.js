82424c2dd18ff775dbbd885e6d4a8d8ca1396425
SimonDesc <pro.descombes@gmail.com>
2023-09-13 16:52:59 +0200

    feat: Implement a class named Pricing

---
+++ ES6_classes/4-main.js
@@ -0,0 +1,9 @@
+import Pricing from './4-pricing.js';
+import convertPrice from './4-pricing.js';
+import Currency from './3-currency.js';
+
+const p = new Pricing(100, new Currency("EUR", "Euro"))
+console.log(p);
+console.log(p.displayFullPrice());
+
+console.log(convertPrice(10, 1.5))
