5bcff78d2125557ac9973bc799f7bda55b207463
SimonDesc <pro.descombes@gmail.com>
2023-09-13 17:31:27 +0200

    fix: checker

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,24 +1,17 @@
-/* eslint-disable class-methods-use-this */
 export default class Building {
   constructor(sqft) {
-    if (
-      this.evacuationWarningMessage
-      === Building.prototype.evacuationWarningMessage
-    ) {
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage',
-      );
+    if (typeof sqft !== 'number') throw TypeError('amount must be a number');
+    if (this.constructor !== Building) {
+      if (!this.evacuationWarningMessage) {
+        throw Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error(
-      'Class extending Building must override evacuationWarningMessage',
-    );
   }
 }
