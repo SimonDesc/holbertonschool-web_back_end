58828eed741e302538eb3796b01b317bb8baeea4
SimonDesc <pro.descombes@gmail.com>
2023-10-02 15:34:52 +0200

    fix

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,9 @@
-import { createUser, uploadPhoto } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promises = [uploadPhoto(), createUser()];
-
-  Promise.all(promises)
-    .then(([photoResult, userResult]) => {
-      console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
