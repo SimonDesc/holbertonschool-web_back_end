aa1f52a7f0c18b558aea139aab22102ab558ae65
SimonDesc <pro.descombes@gmail.com>
2023-10-02 11:18:41 +0200

    fix : Append three handlers to the function:
        When the Promise resolves, return an object with the following attributes
            status: 200
            body: success
        When the Promise rejects, return an empty Error object
        For every resolution, log Got a response from the API to the console

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,5 +1,4 @@
 // Append three handlers to the function:
-
 //     When the Promise resolves, return an object with the following attributes
 //         status: 200
 //         body: success
@@ -7,7 +6,8 @@
 //     For every resolution, log Got a response from the API to the console
 
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'success' }))
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
