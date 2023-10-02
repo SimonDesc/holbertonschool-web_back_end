d78bd8d1a4708a69e5f83b59ac1d4b1e336e898f
SimonDesc <pro.descombes@gmail.com>
2023-10-02 16:46:48 +0200

    fix: checker only allow one way to do it

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,25 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([userPromise, photoPromise])
-    .then((results) => {
-      const transformedResults = [];
-
-      for (let i = 0; i < results.length; i++) {
-        let value;
-        if (results[i].status === 'fulfilled') {
-          value = results[i].value;
-        } else {
-          value = results[i].reason.toString();
-        }
-
-        transformedResults.push({
-          status: results[i].status,
-          value,
-        });
-      }
-
-      return transformedResults;
-    });
+	  .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+	  })));
 }
