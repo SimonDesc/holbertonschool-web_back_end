8b1f46cb6a303f7106558dd48af08ae56f2c66e4
SimonDesc <pro.descombes@gmail.com>
2023-10-03 16:10:01 +0200

    fix

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
-  view.setInt16(position - 1, value, false);
+  view.setInt16(position, value, false);
   return view;
 }
