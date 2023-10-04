ffdeb48a453b2bf5b6cfb9e2c6355f337aa6d7cd
SimonDesc <pro.descombes@gmail.com>
2023-10-04 10:39:50 +0200

    fix: eslint

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,27 +1,27 @@
 export default function cleanSet(Set, startString) {
-	let stringAllSet = '';
+  let stringAllSet = '';
 
-	if (typeof startString !== 'string') {
-		return stringAllSet;
-	}
+  if (typeof startString !== 'string') {
+    return stringAllSet;
+  }
 
-	if (startString === '') {
-		return stringAllSet;
-	}
+  if (startString === '') {
+    return stringAllSet;
+  }
 
-	const strLenght = startString.length;
+  const strLenght = startString.length;
 
-	for (const value of Set) {
-		if (typeof value === 'string') {
-			if (value.search(startString) !== -1) {
-				const startStr = value.search(startString); // position de début
-				stringAllSet += value.substring(startStr + strLenght);
-				stringAllSet += '-';
-			}
-		}
-	}
+  for (const value of Set) {
+    if (typeof value === 'string') {
+      if (value.search(startString) !== -1) {
+        const startStr = value.search(startString); // position de début
+        stringAllSet += value.substring(startStr + strLenght);
+        stringAllSet += '-';
+      }
+    }
+  }
 
-	stringAllSet = stringAllSet.slice(0, -1);
+  stringAllSet = stringAllSet.slice(0, -1);
 
-	return stringAllSet;
+  return stringAllSet;
 }
