3f6dfbd239f302c80e0b7c0778f620dccd8073ac
SimonDesc <pro.descombes@gmail.com>
2024-02-07 10:59:51 +0100

    feat: LRU implementation

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -11,7 +11,8 @@ class LRUCache(BaseCaching):
     """
 
     def __init__(self):
-        return super().__init__()
+        super().__init__()
+        self.order_used_item = []
 
     def put(self, key, item):
         """ Add an item in the cache
@@ -19,15 +20,21 @@ class LRUCache(BaseCaching):
         if key is None or item is None:
             pass
         else:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                lru_item = list(self.cache_data.keys())[-1]
+            if key in self.cache_data:
+                self.order_used_item.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lru_item = self.order_used_item.pop(0)
                 del self.cache_data[lru_item]
                 print("DISCARD:", lru_item)
             self.cache_data[key] = item
+            self.order_used_item.append(key)
 
     def get(self, key):
         """ Get an item by key
         """
         if key is None or (key not in self.cache_data):
             return None
+        if key in self.order_used_item:
+            self.order_used_item.remove(key)
+        self.order_used_item.append(key)
         return self.cache_data[key]
