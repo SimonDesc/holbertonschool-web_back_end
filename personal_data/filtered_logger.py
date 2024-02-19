be3bad5069771868305a3d0c31ea2354059b4652
SimonDesc <pro.descombes@gmail.com>
2024-02-19 19:25:04 +0100

    fix: checker won't allow my first solution

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -9,9 +9,7 @@ import re
 def filter_datum(fields: list[str], redaction: str, message: str, separator: str) -> str:
     """A function used to obfuscate the log message"""
     for field in fields:
-        # Création du motif regex pour trouver le champ et sa valeur
-        pattern = re.compile(r'{}=.*?{}'.format(field, separator))
-        # Remplacement de chaque occurrence du champ par le champ
-        # lui-même et le texte d'offuscation
-        message = pattern.sub(f'{field}={redaction}{separator}', message)
+        message = re.sub(fr'{field}=.+?{separator}',
+                         f'{field}={redaction}{separator}',
+                         message)
     return message
