b37b241b3d856ba01ef8cf062bcd23892bd820d1
SimonDesc <pro.descombes@gmail.com>
2024-02-19 19:27:29 +0100

    doc: pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,7 +7,10 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     """A function used to obfuscate the log message"""
     for field in fields:
         message = re.sub(fr'{field}=.+?{separator}',
