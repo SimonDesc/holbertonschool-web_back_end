733c992b5f974435f3c8e12c0b81929e986aa943
SimonDesc <pro.descombes@gmail.com>
2024-02-20 09:17:21 +0100

    feat: implment the log formatter with loggin module

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 filter_datum
 """
 
+import logging
 from typing import List
 import re
 
@@ -17,3 +18,24 @@ def filter_datum(fields: List[str],
                          f'{field}={redaction}{separator}',
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_message = super().format(record)
+        redacted_message = filter_datum(self.fields,
+                                        self.REDACTION,
+                                        original_message,
+                                        self.SEPARATOR)
+        return redacted_message
