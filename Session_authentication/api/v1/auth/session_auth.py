f9d8ebdf2d743e8cae6d742a03176e67789942ac
SimonDesc <pro.descombes@gmail.com>
2024-02-22 11:38:30 +0100

    feat: implement session auth

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Manage the Session authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Session Authentification
+    """
+    pass
