99553d2e6c13d5ee370048421703296533587ec8
SimonDesc <pro.descombes@gmail.com>
2024-02-22 11:54:08 +0100

    feat: User ID for Session ID

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -23,3 +23,12 @@ class SessionAuth(Auth):
             session_id_str = str(session_id)
             self.user_id_by_session_id[session_id_str] = user_id
             return session_id_str
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns a User ID based on a Session ID
+        """
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
