ca0b84a19b22e32241505d09461c21231c01cda9
SimonDesc <pro.descombes@gmail.com>
2024-02-22 11:46:03 +0100

    feat: Create a session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,10 +2,24 @@
 """
 Manage the Session authentication
 """
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """ Session Authentification
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a new session ID for a user id
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        else:
+            session_id = uuid.uuid4()
+            session_id_str = str(session_id)
+            self.user_id_by_session_id[session_id_str] = user_id
+            return session_id_str
