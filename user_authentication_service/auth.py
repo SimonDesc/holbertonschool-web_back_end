128fbb6056a08d596a95426d8cc24198774d78eb
SimonDesc <pro.descombes@gmail.com>
2024-02-26 15:00:50 +0100

    fix: true, update_user method exist...

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -80,10 +80,4 @@ class Auth:
 
     def destroy_session(self, user_id: int) -> None:
         """Delete the session id of a user"""
-        try:
-            user = self._db.find_user_by(id=user_id)
-        except (InvalidRequestError, NoResultFound):
-            return
-
-        user.session_id = None
-        return None
+        self._db.update_user(user_id, session_id=None)
