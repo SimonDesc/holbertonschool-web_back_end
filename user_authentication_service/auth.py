ef7e192a9944a07c7833c90e64270fae246b1602
SimonDesc <pro.descombes@gmail.com>
2024-02-26 12:30:10 +0100

    feat: Credentials validation

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -35,3 +35,18 @@ class Auth:
             raise ValueError(f'User {email} already exists.')
 
         return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Credentials validation"""
+        exists = True
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            exists = False
+
+        if exists:
+            byte_obj = bytes(password, 'utf-8')
+            if bcrypt.checkpw(byte_obj, user.hashed_password):
+                return True
+
+        return False
