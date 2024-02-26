23f546c22a7c6faa26a0add9a5609241e545d568
SimonDesc <pro.descombes@gmail.com>
2024-02-26 15:28:54 +0100

    fix: rename variable

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -53,8 +53,8 @@ def login():
 def logout():
     """Logout route
     """
-    id = request.cookies.get('session_id')
-    user = AUTH.get_user_from_session_id(id)
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
 
     if user:
         AUTH.destroy_session(user.id)
