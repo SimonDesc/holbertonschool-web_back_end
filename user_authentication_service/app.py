c0ae398e88f117544784ab805e24032d0f8e156a
SimonDesc <pro.descombes@gmail.com>
2024-02-26 15:29:50 +0100

    fix: checker ... no else, only abort

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -51,16 +51,14 @@ def login():
 
 @app.route('/sessions', methods=['DELETE'])
 def logout():
-    """Logout route
-    """
+    """Logs users out"""
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
 
     if user:
         AUTH.destroy_session(user.id)
         return redirect('/')
-    else:
-        abort(403)
+    abort(403)
 
 
 if __name__ == "__main__":
