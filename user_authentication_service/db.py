4a6defb701be9dd5a30e96cd0ceda9c9b2f34cb1
SimonDesc <pro.descombes@gmail.com>
2024-02-26 11:05:57 +0100

    fix: add #!/usr/bin/env python3

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """DB module
 """
 from sqlalchemy import create_engine
@@ -15,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -30,12 +31,9 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Add a new user to the database
-        """
-        new_user = User()
-        new_user.email = email
-        new_user.hashed_password = hashed_password
-        self._session(new_user)
+        """Add a user to db"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
 
-        return new_user
+        return user
