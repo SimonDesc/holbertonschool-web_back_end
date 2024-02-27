b8ffca99cf429d0199815d652f4a5e02491f7a6b
SimonDesc <pro.descombes@gmail.com>
2024-02-27 10:53:00 +0100

    fix: better name of the route..

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -97,7 +97,7 @@ def get_reset_password_token():
         abort(403)
 
 
-@app.route('/update_password', methods=['PUT'], strict_slashes=False)
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
     """Update the password
     """
