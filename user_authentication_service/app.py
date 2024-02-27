f571c817cdb94dda27c32da0bbc9787188e7ebc0
SimonDesc <pro.descombes@gmail.com>
2024-02-27 10:33:42 +0100

    feat: Get reset password token

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -79,5 +79,21 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    email = request.form.get('email')
+
+    if not email:
+        abort(403)
+    try:
+        token = AUTH.get_reset_password_token(email)
+        if token:
+            return jsonify({"email": email, "reset_token": token}), 200
+        else:
+            abort(403)
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
