d675d1ae47d5afaf184b328b0498ea0ef738cfb1
SimonDesc <pro.descombes@gmail.com>
2024-03-18 10:48:09 +0100

    feat: Incrementing values

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,9 +2,25 @@
 """
 Generate a new element in Redis
 """
+from functools import wraps
 import redis
 from typing import Callable, Optional, Union
 from uuid import uuid4, UUID
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Decorator for counting how many times a function
+    has been called """
+
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper for decorator functionality """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -15,6 +31,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the input data in Redis using a
         random key and return the key.
@@ -23,7 +40,7 @@ class Cache:
         self._redis.set(random_key, data)
 
         return random_key
-    
+
     def get(self, key: str,
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """
@@ -53,4 +70,3 @@ class Cache:
         except Exception:
             value = 0
         return value
-    
